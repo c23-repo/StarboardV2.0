3ffacb67bba5eac1b6cbf5cd2b160dc4bb5c6f7a
Piao <wenhaopi@08f8bc6186e1.ant.amazon.com>
2021-06-03 15:11:33 -0700

    Create CreateRooms class to create list of rooms object from json file, ab#236

--- src/com/starboard/CreateRooms.java
+++ src/com/starboard/CreateRooms.java
@@ -1,3 +1,33 @@
-package com.starboard;class CreateRooms {
-    
+package com.starboard;
+
+import java.io.*;
+import java.util.*;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.*;
+
+class CreateRooms {
+    public static List<Room> create() {
+        List<Room> roomsList = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            roomsList = mapper.readValue(
+                new File("resources/rooms/roomtest.json"),
+                new TypeReference<List<Room>>(){}
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // add paths field in the Room object in the roomsList
+        for (Room room : roomsList) {
+            for (String roomName : room.getLinkedRooms()) {
+                for (Room linkedRoom : roomsList) {
+                    if (roomName.equals(linkedRoom.getName())) {
+                        room.setPath(roomName, linkedRoom);
+                    }
+                }
+            }
+        }
+        return roomsList;
+    }
 }
