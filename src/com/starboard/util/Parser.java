aa1034d4d1a85399d6c0e734c5ba728167669c52
sun624 <67940809+sun624@users.noreply.github.com>
2021-06-04 12:43:12 -0700

    Modified parser ab#20, ab#24

--- src/com/starboard/util/Parser.java
+++ src/com/starboard/util/Parser.java
@@ -1,59 +1,95 @@
 package com.starboard.util;
 
+import com.starboard.Room;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-class Parser {
+public class Parser {
 
     private String firstCommand;
 
     private String secondCommand;
 
+    private boolean parseStatus;
 
-    public void parse(String str){
+    private final Room room;
+
+    public Parser(Room room){
+        this.room = room;
+    }
+
+
+    public void parse(String str) {
         //strip filler words from user input
-        List<String> fillerWords = Arrays.asList("to","the","a","an","from","in","inside","out","outside","of");
+        List<String> fillerWords = Arrays.asList("to", "the", "a", "an", "from", "in", "inside", "out", "outside", "of");
         String[] splitWords = str.trim().split(" ");
         List<String> command = new ArrayList<>();
 
-        for (String word : splitWords){
-            if (!fillerWords.contains(word)){
+        //build the command
+        for (String word : splitWords) {
+            if (!fillerWords.contains(word)) {
                 command.add(word);
             }
         }
-        //create synonyms for go command
-        List<String> moveCollection = Arrays.asList("go","move","walk","run","sprint","proceed","pass");
 
-        if (moveCollection.contains(command.get(0))){
-            setFirstCommand("go");
-            setSecondCommand(command.get(1));
+        if (command.size() == 0) {
+            setParseStatus(false);
+
+        } else if (command.size() == 1) {
+            List<String> singleEntryRoomNames = Arrays.asList("bridge","lab","enginebay");
+            List<String> exitCollection = Arrays.asList("exit","leave");
+            //if currentroom is bridge||lab||enginebay, can use exit and leave
+            setFirstCommand("exit");
+            setParseStatus(singleEntryRoomNames.contains(room.getName()) && exitCollection.contains(command.get(0)));
+
+        } else {
+
+            //create synonyms for go command
+            List<String> moveCollection = Arrays.asList("go", "move", "walk", "run", "sprint", "proceed", "pass");
+
+            if (moveCollection.contains(command.get(0))) {
+                setFirstCommand("go");
+                setSecondCommand(command.get(1));
+                setParseStatus(true);
+            }
+            //create synonyms for pick command
+            List<String> pickItemsCollection = Arrays.asList("pick", "pickup", "grab", "get", "take", "catch", "capture", "snag", "occupy", "steal", "seize", "grasp", "snatch");
+
+            if (pickItemsCollection.contains(command.get(0))) {
+                setFirstCommand("pick");
+                setSecondCommand(command.get(1));
+                setParseStatus(true);
+            }
+
+            //create synonyms for drop command
+            List<String> dropItemsCollection = Arrays.asList("drop", "leave", "discard");
+            if (dropItemsCollection.contains(command.get(0))) {
+                setFirstCommand("drop");
+                setSecondCommand(command.get(1));
+                setParseStatus(true);
+            }
+
+            //create synonyms for use command
+            List<String> useItemsCollection = Arrays.asList("use", "kill","show", "display", "exhibit", "display", "shoot", "apply", "fire", "throw", "insert", "turn", "push", "pull", "eat", "utilize");
+
+            if (useItemsCollection.contains(command.get(0))) {
+                setFirstCommand("use");
+                setSecondCommand(command.get(1));
+                setParseStatus(true);
+            }
         }
-        //create synonyms for pick command
-        List<String> pickItemsCollection = Arrays.asList("pick","pickup","grab","get","take","catch","capture","snag","occupy","steal","seize","grasp","snatch");
-
-        if (pickItemsCollection.contains(command.get(0))){
-            setFirstCommand("pick");
-            setSecondCommand(command.get(1));
-        }
-
-        //create synonyms for drop command
-        List<String> dropItemsCollection = Arrays.asList("drop","leave");
-        if(dropItemsCollection.contains(command.get(0))){
-            setFirstCommand("drop");
-            setSecondCommand(command.get(1));
-        }
-
-        //create synonyms for use command
-        List<String> useItemsCollection = Arrays.asList("use","show","display","exhibit","display","shoot","apply","fire","throw","insert","turn","push","pull","eat","utilize");
-
-        if (useItemsCollection.contains(command.get(0))){
-            setFirstCommand("use");
-            setSecondCommand(command.get(1));
-        }
 
 
+    }
 
+    public boolean getParseStatus() {
+        return parseStatus;
+    }
+
+    public void setParseStatus(boolean parseStatus) {
+        this.parseStatus = parseStatus;
     }
 
     public String getFirstCommand() {
