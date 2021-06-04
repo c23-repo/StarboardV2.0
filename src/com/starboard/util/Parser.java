206f5223bdcb83327629d02e95e3e0fd58d65ffe
Piao <wenhaopi@08f8bc6186e1.ant.amazon.com>
2021-06-04 15:16:57 -0700

    Normalize user inputs for inputs validation, ab#254, ab#257

--- src/com/starboard/util/Parser.java
+++ src/com/starboard/util/Parser.java
@@ -49,34 +49,34 @@ public class Parser {
             //create synonyms for go command
             List<String> moveCollection = Arrays.asList("go", "move", "walk", "run", "sprint", "proceed", "pass");
 
-            if (moveCollection.contains(command.get(0))) {
+            if (moveCollection.contains(command.get(0).toLowerCase())) {
                 setFirstCommand("go");
-                setSecondCommand(command.get(1));
+                setSecondCommand(command.get(1).toLowerCase());
                 setParseStatus(true);
             }
             //create synonyms for pick command
             List<String> pickItemsCollection = Arrays.asList("pick", "pickup", "grab", "get", "take", "catch", "capture", "snag", "occupy", "steal", "seize", "grasp", "snatch");
 
-            if (pickItemsCollection.contains(command.get(0))) {
+            if (pickItemsCollection.contains(command.get(0).toLowerCase())) {
                 setFirstCommand("pick");
-                setSecondCommand(command.get(1));
+                setSecondCommand(command.get(1).toLowerCase());
                 setParseStatus(true);
             }
 
             //create synonyms for drop command
             List<String> dropItemsCollection = Arrays.asList("drop", "leave", "discard");
-            if (dropItemsCollection.contains(command.get(0))) {
+            if (dropItemsCollection.contains(command.get(0).toLowerCase())) {
                 setFirstCommand("drop");
-                setSecondCommand(command.get(1));
+                setSecondCommand(command.get(1).toLowerCase());
                 setParseStatus(true);
             }
 
             //create synonyms for use command
             List<String> useItemsCollection = Arrays.asList("use", "kill","show", "display", "exhibit", "display", "shoot", "apply", "fire", "throw", "insert", "turn", "push", "pull", "eat", "utilize");
 
-            if (useItemsCollection.contains(command.get(0))) {
+            if (useItemsCollection.contains(command.get(0).toLowerCase())) {
                 setFirstCommand("use");
-                setSecondCommand(command.get(1));
+                setSecondCommand(command.get(1).toLowerCase());
                 setParseStatus(true);
             }
         }
