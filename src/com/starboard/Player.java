287cc5def31bc5dea2c2836cc386fbabd45ef258
Julian Dahlstet <jdahlstet@hotmail.com>
2021-06-04 13:34:31 -0700

    Created Player class and some item classes. Player has a HashMap inventory that uses String item names for keys and GameItem values. ab#32

---
+++ src/com/starboard/Player.java
@@ -0,0 +1,71 @@
+package com.starboard;
+
+import com.starboard.items.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Player {
+    private int maxHp;
+    private int hp;
+    private final Map<String, GameItem> inventory = new HashMap<>();
+    private Weapon equippedWeapon = null;
+
+    public void use(Usable item) {
+        item.use(this);
+    }
+
+    public void setMaxHp(int maxHp) {
+        this.maxHp = maxHp;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        if (hp <= maxHp && hp >= 0) {
+            this.hp = hp;
+        }
+        else if (hp > maxHp) {
+            this.hp = maxHp;
+        }
+        else {
+            this.hp = 0;
+        }
+    }
+
+    // changes hp by the given int. Positive numbers heal; negative numbers damage.
+    public void changeHp(int change) {
+        if (hp + change <= maxHp && hp + change >= 0) {
+            hp += change;
+        }
+        else if (hp + change > maxHp) {
+            hp = maxHp;
+        }
+        else if (hp + change < 0) {
+            hp = 0;
+        }
+    }
+
+    public Map<String, GameItem> getInventory() {
+        return inventory;
+    }
+
+    public void takeItem(GameItem item) {
+        if (item.isPortable()) {
+            inventory.put(item.getName(), item);
+        } else {
+            System.out.printf("Can't take %s", item.getName());
+        }
+    }
+
+    // Removes an item from inventory and returns it, so it can go into current room. May return null.
+    public GameItem dropItem(String itemName) {
+        try {
+            return inventory.remove(itemName);
+        } catch (NullPointerException e) {
+            System.out.printf("You do not have %s", itemName);
+            return null;
+        }
+    }
+}
