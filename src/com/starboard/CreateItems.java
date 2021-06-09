f3a5522ca0f966f311ee692b236dba3e26a4e200
Piao <wenhaopi@08f8bc6186e1.ant.amazon.com>
2021-06-09 15:05:54 -0700

    Add several weapon items, ab#306, ab#308

--- src/com/starboard/CreateItems.java
+++ src/com/starboard/CreateItems.java
@@ -3,6 +3,7 @@ package com.starboard;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.starboard.items.GameItem;
+import com.starboard.items.Weapon;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,13 +11,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 class CreateWeapons {
-    public static List<GameItem> create() {
-        List<GameItem> weaponsList = new ArrayList<>();
+    public static List<Weapon> create() {
+        List<Weapon> weaponsList = new ArrayList<>();
         ObjectMapper mapper = new ObjectMapper();
         try {
             weaponsList = mapper.readValue(
                 new File("resources/json/weapons.json"),
-                new TypeReference<List<GameItem>>(){}
+                new TypeReference<List<Weapon>>(){}
             );
         } catch (IOException e) {
             e.printStackTrace();
