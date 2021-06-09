9998e407e0720c0d2411975be2606431c37183a2
Piao <wenhaopi@08f8bc6186e1.ant.amazon.com>
2021-06-09 15:16:03 -0700

    Add createHealingItems static method, ab#323, ab#306

--- src/com/starboard/CreateItems.java
+++ src/com/starboard/CreateItems.java
@@ -2,7 +2,7 @@ package com.starboard;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.starboard.items.GameItem;
+import com.starboard.items.HealingItem;
 import com.starboard.items.Weapon;
 
 import java.io.File;
@@ -10,8 +10,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-class CreateWeapons {
-    public static List<Weapon> create() {
+class CreateItems {
+    public static List<Weapon> createWeapons() {
         List<Weapon> weaponsList = new ArrayList<>();
         ObjectMapper mapper = new ObjectMapper();
         try {
@@ -23,5 +23,19 @@ class CreateWeapons {
             e.printStackTrace();
         }
         return weaponsList;
+    }
+
+    public static List<HealingItem> createHealingItems() {
+        List<HealingItem> healingItemsList = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            healingItemsList = mapper.readValue(
+                    new File("resources/json/healingItems.json"),
+                    new TypeReference<List<HealingItem>>(){}
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return healingItemsList;
     }
 }
