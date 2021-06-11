5e46327a03c3e3a13cdb0f5394d360b72bcc60b9
sun624 <67940809+sun624@users.noreply.github.com>
2021-06-11 15:47:06 -0700

    create method for levels ab#302, ab#355 rebalance the damage and heal value for items in the game.

--- src/com/starboard/Game.java
+++ src/com/starboard/Game.java
@@ -7,6 +7,7 @@ import com.starboard.util.Prompt;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Game {
     private static Room currentRoom;
@@ -16,6 +17,8 @@ public class Game {
 
     public static void start() {
         Prompt.showWelcome();
+        int alienNumber = chooseLevel();
+
         boolean endGame = false;
         // create weapons list
         List<Weapon> weaponsList = CreateItems.createWeapons();
@@ -44,13 +47,22 @@ public class Game {
 
         // Initialize start room
         currentRoom = roomsList.get(0);
+
+        Prompt.showMap();
+
         // show commands
         Prompt.showCommands();
-        Prompt.showMap();
+
+        //Training mode
+        if (alienNumber == 0){
+
+            training();
+        }
+        Prompt.showIntroduction();
 
         //initialize player
         Player player = new Player();
-        Alien aliens = new Alien(100,5);
+        Alien aliens = new Alien(100, alienNumber);
 
         while (!endGame) {
 
@@ -82,6 +94,27 @@ public class Game {
                 endGame = true;
             }
         }
+    }
+
+    private static void training() {
+        System.out.println("Entering training mode");
+
+    }
+
+    public static int chooseLevel(){
+        System.out.println("Please choose from following levels\n" +
+                "0. \t\t\t\tTraining\n"+
+                "1. \t\t\t\tEasy\n"+
+                "2. \t\t\t\tIntermediate\n"+
+                "3. \t\t\t\tHard\n"+
+                "4. \t\t\t\tHell");
+        System.out.print("Enter the number\n>");
+        Scanner sc = new Scanner(System.in);
+
+        int choice = sc.nextInt();
+        return choice * 2;
+
+
     }
 
     public static Room getCurrentRoom() {
