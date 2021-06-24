2cb79122af639a893d97d8c0d7436886a1505ad7
gtulkn12 <gtulkn1@yahoo.com>
2021-06-24 12:18:38 -0400

    added introtext File, TextArea loads introtext File on game Start #AB470

--- src/main/java/com/gui/ControllerMainScene.java
+++ src/main/java/com/gui/ControllerMainScene.java
@@ -10,7 +10,10 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 public class ControllerMainScene implements Initializable {
@@ -27,6 +30,12 @@ public class ControllerMainScene implements Initializable {
     Button btnNewGame;
 
     private String currentInput;
+
+    private final InputSignal inputSignal = new InputSignal();
+
+    public static class InputSignal {
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -51,14 +60,49 @@ public class ControllerMainScene implements Initializable {
                     if (keyEvent.getCode() == KeyCode.ENTER) {
                         currentInput = getPlayerInput().getText().trim();
                         gameTextArea.setText(currentInput);
+                        System.out.println(currentInput);
                         getPlayerInput().clear();
                         getPlayerInput().requestFocus();
                     }
                 };
 
         getPlayerInput().setOnKeyPressed(enterPressedHandler);
-
         getBtnUserInput().setOnAction(eventHandler);
+        updateGameTextArea();
+    }
+
+    public String getInput() {
+        waitInput();
+        return currentInput;
+    }
+
+    public void notifyInput() {
+        synchronized (inputSignal) {
+            inputSignal.notify();
+        }
+    }
+
+    public void waitInput() {
+        synchronized (inputSignal) {
+            try {
+                inputSignal.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void updateGameTextArea() {
+        String path = "resources/welcome/introtext.txt";
+        String banner = null;
+        try {
+            banner = Files.readString(Paths.get(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        gameTextArea.setText(banner);
+//        String text = gameTextArea.getText();
+//        getGameTextArea().appendText(text);
     }
 
     public TextField getPlayerInput() {
@@ -67,5 +111,9 @@ public class ControllerMainScene implements Initializable {
 
     public Button getBtnUserInput() {
         return btnUserInput;
+    }
+
+    TextArea getGameTextArea(){
+        return gameTextArea;
     }
 }
