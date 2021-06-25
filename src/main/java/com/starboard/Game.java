2917ff40caf3054c1d83e4b061231527fb6ba09f
Cristian Restrepo <crestrepo21@gmail.com>
2021-06-25 13:27:01 -0700

    four new weapon Upgrades created in game and updated AB#468

--- src/main/java/com/starboard/Game.java
+++ src/main/java/com/starboard/Game.java
@@ -23,13 +23,13 @@ public class Game {
     public static boolean soundOn = true;
 
     public static void main(String[] args) {
-//        setGameMusic(Music.backgroundMusic);
-        Prompt.showWelcome();
-        soundControl();
-        Prompt.showInstructions();
-        start();
-//        init();
-//        Main.main(args);
+        setGameMusic(Music.backgroundMusic);
+//        Prompt.showWelcome();
+//        soundControl();
+//        Prompt.showInstructions();
+//        start();
+        init();
+        Main.main(args);
     }
 
     public static void init() {
@@ -73,6 +73,7 @@ public class Game {
             init();
             alienNumber = chooseLevel();
         }
+        Prompt.showIntroduction();
         Prompt.showIntroduction();
 
         //initialize player
@@ -253,5 +254,4 @@ public class Game {
     public static int getAlienNumber() {
         return alienNumber;
     }
-
 }
